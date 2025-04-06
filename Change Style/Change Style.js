c3caa39a63b29b60d7e2093644327304da433b67
Jhunalyn N. Pegad <202411795@gordoncollege.edu.ph>
2025-04-06 22:01:19 +0800

    Edit Change

---
+++ Change Style/Change Style.js
@@ -0,0 +1,6 @@
+function change() {
+    var element = document.getElementById("change");
+    element.style.backgroundColor = "coral";
+    element.style.color = "black";
+    element.style.border = "2px solid orangered";
+}
---
+++ Change Text/Change Text.js
@@ -0,0 +1,11 @@
+function change() {
+    var element = document.getElementById("change"); 
+    element.addEventListener('click');
+}
+function change() {
+    const element = document.getElementById("change");
+    element.innerHTML = "Human-Computer Interaction";
+    element.style.fontFamily = "Trebuchet MS";
+    element.style.color = "black";
+    element.style.fontSize = '30px';
+}
